44e2f6fec7f45e514238d83e32daa0c9a27dbeff
Ramim-12 <ramim.hussain12@stu.bmcc.cuny.edu>
2026-05-12 16:14:19 -0400

    Simplify main function in main.cpp
    Removed unnecessary srand and restart loop for game.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,14 @@
 #include "TicTacToe.h"
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 
 using namespace std;
 
 int main() {
-    srand(static_cast<unsigned int>(time(nullptr))); // seed once at startup
-
     GameSignal signal = GameSignal::MENU;
 
     while (signal != GameSignal::QUIT) {
         TicTacToe game;
-
-        do {
-            signal = game.playGame();
-        } while (signal == GameSignal::RESTART);
-
-        // if MENU ? loop continues (new game created)
-        // if QUIT ? loop exits
+        signal = game.playGame();
     }
 
     cout << "\nThanks for playing! Goodbye.\n";
